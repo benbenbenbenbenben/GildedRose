48e0d03632a4974810863e829e4c964a4b1fcf9e
Benjamin Babik <benny@curlyben.com>
2017-02-14 12:31:50 +0000

    updated

---
+++ GildedRose.Scenarios/SpecFlowFeature1.Steps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GildedRose.Console;
+using TechTalk.SpecFlow;
+
+namespace GildedRose.Scenarios
+{
+    [Binding]
+    public class SpecFlowFeature1
+    {
+        [Given(@"I create Program")]
+        public void GivenICreateProgram()
+        {
+            var program = new Program();
+        }
+
+    }
+}
